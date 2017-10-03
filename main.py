759861d4fe084eb3b3b16b9277dc0b5eeec0d157
Changchun Ye <chchye@nudt.edu.cn>
2017-10-03 15:41:31 +0800

    实现了： 1.  修改了窗口尺寸。 2. 绘制10X20的表格。
    下一步：
    1. 绘制一个小块。

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import sys
 import pygame
 
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 900
+CELL_WIDTH = 40
+WORK_AREA_WIDTH = CELL_WIDTH * 10
+WORK_AREA_HEIGHT = CELL_WIDTH * 20
+WORK_AREA_LEFT = (SCREEN_WIDTH - WORK_AREA_WIDTH) // 2
+WORK_AREA_TOP = SCREEN_HEIGHT - WORK_AREA_HEIGHT
+EDEG_COLOR = (0, 0, 0)
+
 def drawWorkArea(screen):
-    pygame.draw.line(screen, (0, 0, 0), (100, 100), (200, 200))
+    #pygame.draw.line(screen, (0, 0, 0), (100, 100), (200, 200))
+    for r in range(21):
+        pygame.draw.line(screen, EDEG_COLOR, (WORK_AREA_LEFT, WORK_AREA_TOP + r * CELL_WIDTH),
+                         (WORK_AREA_LEFT + WORK_AREA_WIDTH, WORK_AREA_TOP + r * CELL_WIDTH))
+    for c in range(11):
+        pygame.draw.line(screen, EDEG_COLOR, (WORK_AREA_LEFT + c * CELL_WIDTH, WORK_AREA_TOP),
+                         (WORK_AREA_LEFT + c * CELL_WIDTH, WORK_AREA_TOP + WORK_AREA_HEIGHT))
 
 def main():
     #初始化pygame
     pygame.init()
     #创建屏幕对象
-    screen = pygame.display.set_mode((1200, 800) )
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT) )
     pygame.display.set_caption("俄罗斯方块")
 
     #屏幕背景色
